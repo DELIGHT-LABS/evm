586be2421ee8a8aa5e973469bf2ca41fe6d21ec3
nowooj <joowon@delightlabs.io>
2026-09-16 10:55:50 +0900

    test(rpc): fix transaction rejection test lint
    (cherry picked from commit dc551f538435f4648c36c93a4c861b024f3ebb3b)

--- rpc/backend/tx_reject_test.go
+++ rpc/backend/tx_reject_test.go
@@ -197,11 +197,11 @@ func admissionJSONResponse(t *testing.T, server *rpc.Server, raw []byte) map[str
 func TestSendRawTransactionAdmissionTransport(t *testing.T) {
 	configurator := evmtypes.NewEVMConfigurator()
 	configurator.ResetTestConfig()
-require.NoError(t,
-        configurator.
-                WithEVMCoinInfo(constants.ChainsCoinInfo[constants.ExampleChainID.EVMChainID]).
-                Configure(),
-  )
+	require.NoError(t,
+		configurator.
+			WithEVMCoinInfo(constants.ChainsCoinInfo[constants.ExampleChainID.EVMChainID]).
+			Configure(),
+	)
 	require.NoError(t, evmtypes.SetChainConfig(evmtypes.DefaultChainConfig(constants.ExampleChainID.EVMChainID)))
 	t.Cleanup(configurator.ResetTestConfig)
 	backend := setupMockBackend(t)
